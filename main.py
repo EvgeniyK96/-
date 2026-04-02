db72bae9e601d32451d814e2aebf08b3dedcd71a
EvgeniyK96 <kureda96@gmail.com>
2026-04-02 17:22:59 +0500

    записан код

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hallo")
+temp = int(input("Какая на улице температура?: "))
+if temp <= 5:
+    print(f"На улице {temp}°С, холодно")
+elif temp > 5 and temp <= 18:
+    print(f"На улице {temp}°С, тепло!!!")
+else:
+    print(f"На улице {temp}°С , жарко")
