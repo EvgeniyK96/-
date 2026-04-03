339f3b912c62b85d73810e48d7cf053dc274ba88
EvgeniyK96 <kureda96@gmail.com>
2026-04-03 17:44:48 +0500

    добавлены новые условия для определения погоды

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 temp = int(input("Какая на улице температура?: "))
-if temp <= 5:
-    print(f"На улице {temp}°С, холодно")
-elif temp > 5 and temp <= 18:
-    print(f"На улице {temp}°С, тепло!!!")
+rain = input("На улице идет дождь? Да\Нет: ")
+r = False
+if rain == "да" or rain == "Да":
+    r = True
+
+if temp <= 5 and r:
+    print(f"На улице {temp}°С, холодно и дождливо, одевайся теплее!!!")
+elif temp <= 5 and not r:
+    print(f"На улице {temp}°С, холодно, одевайся теплее!!!")
+elif temp > 5 and temp <= 18 and r:
+    print(f"На улице {temp}°С, тепло, но не забудь зонт")
+elif temp > 5 and temp <= 18 and not r:
+    print(f"На улице {temp}°С, тепло, но не забудь зонт")
+elif temp > 18 and r:
+    print(f"На улице {temp}°С, жарко, но не забудь зонт")
 else:
     print(f"На улице {temp}°С , жарко")
